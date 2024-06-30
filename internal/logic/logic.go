91202ce14ec6a717628a6a0c5ceb5e028d24d5c1
Stone-afk <1711865140@qq.com>
2024-06-30 20:24:17 +0800

    feature: add glogin

--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -1,5 +1,11 @@
 package logic
 
 import (
+	_ "goframe_shop/internal/logic/admin"
+	_ "goframe_shop/internal/logic/bizctx"
 	_ "goframe_shop/internal/logic/goods"
+	_ "goframe_shop/internal/logic/middleware"
+	_ "goframe_shop/internal/logic/rotation"
+	_ "goframe_shop/internal/logic/session"
+	_ "goframe_shop/internal/logic/user"
 )
--- internal/logic/middleware/middleware.go
+++ internal/logic/middleware/middleware.go
@@ -114,11 +114,13 @@ func (s *sMiddleware) Ctx(r *ghttp.Request) {
 }
 
 func (s *sMiddleware) CORS(r *ghttp.Request) {
-
+	r.Response.CORSDefault()
+	r.Middleware.Next()
 }
 
 func (s *sMiddleware) Auth(r *ghttp.Request) {
-
+	service.Auth().MiddlewareFunc()(r)
+	r.Middleware.Next()
 }
 
 var GToken *gtoken.GfToken
